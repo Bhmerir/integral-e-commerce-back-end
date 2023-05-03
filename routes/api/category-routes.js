810695bf7e611c3e8b2abed88d87a3c7e47d6e74
Bahareh <mer_ir@yahoo.com>
2023-05-02 20:53:23 -0700

    add get routes for getting all tages and getting tag by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,33 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+//This get finds all categories and includes its associated products
+router.get('/', async (req, res) => {
+  try{
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async(req, res) => {
+  try{
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    });
+
+    if(!categoryData){
+      res.status(404).json({message: 'No tag found with that id!'});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
